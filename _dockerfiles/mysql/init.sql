7694a2c34b67d2db69b5c8a967a82de02089ef9f
denOldTimer <kribo@outlook.com>
2023-05-23 14:57:47 +0200

    Mod MySql init file

--- _dockerfiles/mysql/init.sql
+++ _dockerfiles/mysql/init.sql
@@ -11,3 +11,16 @@ CREATE TABLE IF NOT EXISTS accounts (
   psw_hash VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT NOW()
 );
+
+
+CREATE TABLE `posts` (
+  `id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `status` enum('draft','published','archived') NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `content` longtext NOT NULL
+);
+
+INSERT INTO `posts` (`id`, `title`, `slug`, `status`, `author`, `img`, `content`) VALUES (NULL, 'Test one draft version', 'test-one-draft-version', 'draft', 'kribo', '', 'THIS IS TEST ONE '), (NULL, 'Test two publised version', 'test-two-published-version', 'published', 'kribo', '', 'THIS IS TEST TWO ');
